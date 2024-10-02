dc486b806852e8f808db2236a020f1879ebc6fb7
Craig <craig.hickman@ukaea.uk>
2024-10-02 18:27:09 +0100

    commit

--- examples/2d_shapes_pbr_cache.rs
+++ examples/2d_shapes_pbr_cache.rs
@@ -36,9 +36,18 @@ fn spawn_entities(
 
     cmds.spawn_2d_camera(Args::default());
 
+    let mesh_h = mesh_cache.0.get("10x10rect").unwrap();
+    let mat_h = mat_cache.0.get("red").unwrap();
+
     cmds.spawn_square(Args{
-        mesh: Some(GetHandle::Reuse(mesh_cache.0.get("10x10rect").unwrap().clone())),
-        mat: Some(GetHandle::Reuse(mat_cache.0.get("red").unwrap().clone())),
+        mesh: Some(GetHandle::Reuse(mesh_h.clone())),
+        mat: Some(GetHandle::Reuse(mat_h.clone())),
+        ..default()
+    });
+
+    cmds.spawn_square(Args{
+        mesh: Some(GetHandle::Reuse(mesh_h.clone())),
+        mat: Some(GetHandle::Reuse(mat_h.clone())),
         ..default()
     });
 }
